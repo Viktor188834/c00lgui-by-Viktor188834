bdb276ad56c014121a0f453535cdf6277989d9dc
Viktor188834 <atabaevviktor671@gmail.com>
2025-06-20 10:09:04 +0300

    Update dadada.lua

--- dadada.lua
+++ dadada.lua
@@ -27,6 +27,18 @@ SecondFrameButton.Active = true
 SecondFrameButton.Text = "Toggle ESP"
 SecondFrameButton.TextColor3 = Color3.fromRGB(255, 255, 255)
 
+local GodCoil = Instance.new("TextButton")
+GodCoil.Name = "GodCoil"
+GodCoil.Parent = Frame
+GodCoil.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+GodCoil.BorderColor3 = Color3.fromRGB(255, 0, 0)
+GodCoil.BorderSizePixel = 4
+GodCoil.Position = UDim2.new(0.0900000036, 0, 0.3799999982, 0)
+GodCoil.Size = UDim2.new(0.360000014, 0, 0.0700000003, 0)
+GodCoil.Active = true
+GodCoil.Text = "GodCoil!"
+GodCoil.TextColor3 = Color3.fromRGB(255, 255, 255)
+
 local Akkk = Instance.new("TextButton")
 Akkk.Name = "Ak47"
 Akkk.Parent = Frame
@@ -96,7 +108,7 @@ FirstText.TextSize = 12
 FirstText.TextColor3 = Color3.fromRGB(255, 255, 255)
 
 local DeleteGui = Instance.new("TextButton")
-DeleteGui.Parent = Frame
+DeleteGui.Parent = MainGui
 DeleteGui.Name = "DeleteGui"
 DeleteGui.TextColor3 = Color3.fromRGB(255, 255, 255)
 DeleteGui.Text = "Del"
@@ -104,8 +116,8 @@ DeleteGui.TextSize = 8
 DeleteGui.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 DeleteGui.BorderColor3 = Color3.fromRGB(255, 0, 0)
 DeleteGui.BorderSizePixel = 3
-DeleteGui.Size = UDim2.new(0.070000014, 0, 0.0600000003, 0)
-DeleteGui.Position = UDim2.new(0.91, 0, 0.93, 0)
+DeleteGui.Size = UDim2.new(0.02000014, 0, 0.020000003, 0)
+DeleteGui.Position = UDim2.new(0.979, 0, 0.977, 0)
 
 local HideGui = Instance.new("TextButton")
 HideGui.Parent = Frame
@@ -117,7 +129,7 @@ HideGui.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 HideGui.BorderColor3 = Color3.fromRGB(255, 0, 0)
 HideGui.BorderSizePixel = 3
 HideGui.Size = UDim2.new(-0.060000014, 0, 0.0700000003, 0)
-HideGui.Position = UDim2.new(-0.01, 0, -0.01, 0)
+HideGui.Position = UDim2.new(-0.01, 0, -0.013, 0)
 local hideorvisiblemoyscript = true
 
 local function VisibleGuiScript()
@@ -187,9 +199,11 @@ local function VisibleGuiScript()
 	Frame.Position = UDim2.new(0.99, 0, 0.25, 0)
 	task.wait(0.05)
 	Frame.Position = UDim2.new(1, 0, 0.25, 0)
+	Frame.BorderSizePixel = 0
 end
 
 local function HidenGuiScript()
+	Frame.BorderSizePixel = 10
 	Frame.Position = UDim2.new(0.99, 0, 0.25, 0)
 	task.wait(0.05)
 	Frame.Position = UDim2.new(0.985, 0, 0.25, 0)
@@ -292,8 +306,9 @@ local function toggleEspGenerator()
             	if generator ~= game.Players.LocalPlayer.Character then
                 	local generatotts = Instance.new("Highlight",generator)
            	    	generatotts.Name = "EspBox"
-					generatotts.FillColor = Color3.fromRGB(0, 255, 0)
-      	      end
+					generatotts.FillColor = Color3.fromRGB(255, 0, 0)
+					generatotts.OutlineTransparency = 0.3
+      	        end
     	    end
     	end
 	end
@@ -307,7 +322,21 @@ local function toggleESP()
             	if childrik ~= game.Players.LocalPlayer.Character then
                 	local esp = Instance.new("Highlight",childrik)
            	    	esp.Name = "EspBox"
-					esp.FillColor = Color3.fromRGB(255, 0, 0)
+					esp.FillColor = Color3.fromRGB(125, 0, 125)
+					esp.OutlineTransparency = 0.3
+      	      end
+    	    end
+    	end
+	end
+	for i, animatronik in ipairs(workspace:GetDescendants()) do
+    	if animatronik:FindFirstChild("Aura") then
+        	if not animatronik:FindFirstChild("EspBox") then
+            	if animatronik ~= game.Players.LocalPlayer.Character then
+                	local espa = Instance.new("Highlight")
+					espa.Parent = animatronik.Parent
+           	    	espa.Name = "EspBox"
+					espa.FillColor = Color3.fromRGB(255, 0, 0)
+					espa.OutlineTransparency = 0.3
       	      end
     	    end
     	end
@@ -446,6 +475,130 @@ function SpeedCoil()
 	end
 end
 
+function GodCoill()
+	function sandbox(var,func)
+		local env = getfenv(func)
+		local newenv = setmetatable({},{
+			__index = function(self,k)
+				if k=="script" then
+					return var
+				else
+					return env[k]
+				end
+			end,
+		})
+		setfenv(func,newenv)
+		return func
+	end
+	cors = {}
+	mas = Instance.new("Model",game:GetService("Lighting"))
+	Tool0 = Instance.new("Tool")
+	Script1 = Instance.new("Script")
+	Part2 = Instance.new("Part")
+	SpecialMesh3 = Instance.new("SpecialMesh")
+	Sound4 = Instance.new("Sound")
+	Tool0.Name = "SpeedCoil"
+	Tool0.Parent = mas
+	Tool0.TextureId = "rbxassetid://99170415"
+	Tool0.Grip = CFrame.new(0, 0, -0.800000012, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	Tool0.GripPos = Vector3.new(0, 0, -0.800000011920929)
+	Tool0.ToolTip = "God Speed Up"
+	Script1.Name = "SpeedScript"
+	Script1.Parent = Tool0
+	table.insert(cors,sandbox(Script1,function()
+		--Rescripted by samtheblender
+		local Services = {
+			Players = (game:FindService("Players") or game:GetService("Players")),
+			TweenService = (game:FindService("TweenService") or game:GetService("TweenService")),	
+			Debris = (game:FindService("Debris") or game:GetService("Debris")),
+			RunService = (game:FindService("RunService") or game:GetService("RunService"))
+		}
+
+		local Properties = {
+			SpeedBoost = 25,
+			SpeedForSmoke = 40
+		}
+
+		local Tool = script.Parent
+		local Handle = Tool:WaitForChild("Handle")
+		local Character, Player, Humanoid, Root, LastSpeed, SmokeConnection, SmokeEffect
+
+		function CheckIfAlive()
+			return (((Character and Character.Parent and Humanoid and Humanoid.Parent and Humanoid.Health > 0 and Player and Player.Parent) and true) or false)
+		end
+
+		function ClearEffect()
+			if SmokeEffect then
+				SmokeEffect.Enabled = false
+				Services.Debris:AddItem(SmokeEffect, 5)
+				SmokeEffect = nil
+			end
+		end
+		Tool.Equipped:Connect(function()
+			Character = Tool.Parent
+			Player = Services.Players:GetPlayerFromCharacter(Character)
+			Humanoid = Character:FindFirstChildOfClass("Humanoid")
+			Root = Character:FindFirstChild("HumanoidRootPart")
+			Services.RunService.Heartbeat:Wait()
+			if not Tool:IsDescendantOf(workspace) or not CheckIfAlive() or LastSpeed then
+				return
+			end
+			LastSpeed = Humanoid.WalkSpeed
+			Humanoid.WalkSpeed = Humanoid.WalkSpeed + Properties.SpeedBoost
+			Handle:WaitForChild("Equip"):Play()
+			SmokeConnection = Humanoid.Running:Connect(function(Speed)
+				if Speed >= Properties.SpeedForSmoke then
+					if not SmokeEffect then
+						SmokeEffect = Instance.new("Smoke")
+						SmokeEffect.Parent = Root
+					end
+				else
+					ClearEffect()
+				end
+			end)
+		end)
+
+		Tool.Unequipped:Connect(function()
+			if SmokeConnection then
+				SmokeConnection:Disconnect()
+				SmokeConnection = nil
+			end
+			ClearEffect()
+			if not LastSpeed or not CheckIfAlive() then
+				return
+			end
+			Humanoid.WalkSpeed = LastSpeed
+			LastSpeed = nil
+		end)
+	end))
+	Part2.Name = "Handle"
+	Part2.Parent = Tool0
+	Part2.CFrame = CFrame.new(-3, 0.5, 6, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+	Part2.Position = Vector3.new(-3, 0.5, 6)
+	Part2.Size = Vector3.new(1, 1, 2)
+	Part2.BottomSurface = Enum.SurfaceType.Smooth
+	Part2.TopSurface = Enum.SurfaceType.Smooth
+	SpecialMesh3.Parent = Part2
+	SpecialMesh3.MeshId = "rbxassetid://16606212"
+	SpecialMesh3.Scale = Vector3.new(0.699999988079071, 0.699999988079071, 0.699999988079071)
+	SpecialMesh3.TextureId = "rbxassetid://99170547"
+	SpecialMesh3.MeshType = Enum.MeshType.FileMesh
+	Sound4.Name = "Equip"
+	Sound4.Parent = Part2
+	Sound4.SoundId = "rbxassetid://99173388"
+	Sound4.Volume = 1
+	for i,v in pairs(mas:GetChildren()) do
+		v.Parent = game:GetService("Players").LocalPlayer.Backpack
+		pcall(function() v:MakeJoints() end)
+	end
+	mas:Destroy()
+	for i,v in pairs(cors) do
+		spawn(function()
+			pcall(v)
+		end)
+	end
+end
+
 function Ak47()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/sinret/rbxscript.com-scripts-reuploads-/main/ak47", true))() 
 end
@@ -459,3 +612,4 @@ DeleteGui.MouseButton1Click:Connect(DeleteAllGui)
 DexFrame.MouseButton1Click:Connect(Dex)
 SpeedCoilFrame.MouseButton1Click:Connect(SpeedCoil)
 Akkk.MouseButton1Click:Connect(Ak47)
+GodCoil.MouseButton1Click:Connect(GodCoill)
